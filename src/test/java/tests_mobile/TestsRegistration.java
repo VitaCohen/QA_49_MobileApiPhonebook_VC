68dfb4e10e51d8beeb49c108df6a6c1c1781538c
Vita <cohen0402@gmail.com>
2025-12-07 21:39:35 +0200

    HW_15 registration negative tests

--- src/test/java/tests_mobile/TestsRegistration.java
+++ src/test/java/tests_mobile/TestsRegistration.java
@@ -9,22 +9,20 @@ import screens.ContactListScreen;
 import screens.ErrorScreen;
 import screens.SplashScreen;
 
-import  static utils.UserFactory.*;
+import static utils.UserFactory.*;
 
-public class TestsRegistration extends TestBase{
+public class TestsRegistration extends TestBase {
     AuthenticationScreen authenticationScreen;
 
     @BeforeMethod
-    public  void goToAuthenticationScreen(){
+    public void goToAuthenticationScreen() {
         new SplashScreen(driver);
         authenticationScreen = new AuthenticationScreen(driver);
 
     }
 
-
-
     @Test
-    public void testRegistrationPositive(){
+    public void testRegistrationPositive() {
         User user = positiveUser();
         authenticationScreen.typeAuthForm(user);
         authenticationScreen.clickBtnRegistration();
@@ -33,14 +31,44 @@ public class TestsRegistration extends TestBase{
     }
 
     @Test
-    public void testRegistrationNegative_emptyEmail(){
+    public void testRegistrationNegative_emptyEmail() {
         User user = positiveUser();
         user.setUsername("");
         authenticationScreen.typeAuthForm(user);
         authenticationScreen.clickBtnRegistration();
-       Assert.assertTrue(new ErrorScreen(driver).validateErrorText("username=must not be blank", 10));
+        Assert.assertTrue(new ErrorScreen(driver).validateErrorText("username=must not be blank", 10));
     }
 
+//HW_15
 
+    //@@
+    @Test
+    public void testRegistrationNegative_wrongEmail() {
+        User user = positiveUser();
+        user.setUsername("asq@@m.com");
+        authenticationScreen.typeAuthForm(user);
+        authenticationScreen.clickBtnRegistration();
+        Assert.assertTrue(new ErrorScreen(driver).validateErrorText("username=must be a well-formed email address", 10));
+    }
+
+    // 7 char
+    @Test
+    public void testRegistrationNegative_wrongPassword() {
+        User user = positiveUser();
+        user.setPassword("1234Aa!");
+        authenticationScreen.typeAuthForm(user);
+        authenticationScreen.clickBtnRegistration();
+        Assert.assertTrue(new ErrorScreen(driver).validateErrorText("password= At least 8 characters;", 10));
+    }
+
+    // WO letters
+    @Test
+    public void testRegistrationNegative_wrongPassword_WO_Letters() {
+        User user = positiveUser();
+        user.setPassword("1234567!");
+        authenticationScreen.typeAuthForm(user);
+        authenticationScreen.clickBtnRegistration();
+        Assert.assertTrue(new ErrorScreen(driver).validateErrorText("password= At least 8 characters;", 10));
+    }
 
 }
